d632417c08b39472170b39d34a1fcc504cb073cf
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-09-06 17:11:11 +0400

    Remove unused variable

--- src/dependencies.php
+++ src/dependencies.php
@@ -8,7 +8,7 @@ $c->set(Config::class, static function () {
     return new Config(require 'config.php');
 });
 // Контейнер с шаблонами
-$c->set(View::class, static function () use ($c, $rootDirectoryWebApplication) {
+$c->set(View::class, static function () use ($c) {
     $view = new View($c->get(Config::class)->getViewDirectory());
     // Добавляю глобальную переменную $csrf в шаблоны
     $view->addGlobalData('csrf', new Csrf());
